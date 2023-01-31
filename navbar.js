63709675402b1dc911b5ca500aeb25870ad380d7
david-salomon-zuniga <salomon1142608010@gmail.com>
2023-01-31 16:33:58 -0600

    repairing

--- navbar.js
+++
@@ -1,7 +0,0 @@
-const toggleButton=document.getElementsByClassName('toggle-button')[0];
-const navbarLinks=document.getElementsByClassName('navbar-links')[0];
-const divLogo=document.getElementById('logo');
-    
-toggleButton.addEventListener('click', ()=>{
-        navbarLinks.classList.toggle('active');
-});
